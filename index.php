572394a2c09272bc4e06fa6a9663618ef3c137f5
Francisco Campos <franciiscocampos170@gmail.com>
2020-07-05 13:37:18 +0100

    arrow function basics

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/** [1] - Normal function */
+$greet = function () {
+    return "Hey";
+};
+
+/**[1] - Arrow function */
+$message = fn ($name) => "Hello ". $name;
+//var_dump($message("Francisco"));
+
+
+/**[2] - Normal function */
+$users = [['id' => 1, 'name' => 'Carlos'], ['id' => 2, 'Abgail']];
+
+$ids = array_map(function($user){
+    return $user['id'];
+}, $users);
+
+/**[2] - Arrow function */
+$user_ids = array_map(fn ($user) => $user['id'], $users);
+var_dump($user_ids);
