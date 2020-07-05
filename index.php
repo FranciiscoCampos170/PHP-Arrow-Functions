dfd15b5d5825388e86488e8b137f4446777b599e
Francisco Campos <franciiscocampos170@gmail.com>
2020-07-05 13:47:29 +0100

    how the scopes change

--- index.php
+++ index.php
@@ -19,4 +19,17 @@ $ids = array_map(function($user){
 
 /**[2] - Arrow function */
 $user_ids = array_map(fn ($user) => $user['id'], $users);
-var_dump($user_ids);
+//var_dump($user_ids);
+
+/**[3] - Normal function */
+$name = 'Alex';
+
+$sayHello = function () use ($name)
+{
+    return 'Hello '. $name;
+};
+
+/**[3] - Arrow function */
+$sayHelloTo = fn() => 'Hello '. $name;
+
+var_dump($sayHelloTo());
